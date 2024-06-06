f1b8945241300f4c5424cf2f48162a4503f0c5eb
Raj Kishan <68591610+rajkishancode@users.noreply.github.com>
2024-06-06 07:18:52 +0530

    846. Hand of Straights

--- 1002. Find Common Characters/1002. Find Common Characters.js
+++ 1002. Find Common Characters/1002. Find Common Characters.js
@@ -9,20 +9,52 @@
  * @param {string[]} words
  * @return {string[]}
  */
-var commonChars = function (words) {
-  let charCount = {};
-  for (let char of words[0]) {
-    charCount[char] = (charCount[char] || 0) + 1;
-  }
-  let wordCount;
-  for (let i = 0; i < words.length; i++) {
-    wordCount = {};
-    for (let char of words[i]) {
-      wordCount[char] = (wordCount[char] || 0) + 1;
-    }
-  }
-  console.log(charCount);
-  console.log(wordCount);
-};
 
+var commonChars = function(words) {
+    
+    let charFreqs = new Map();
+    // initialize charFreqs with the first word
+    
+    for (let char of words[0]) {
+        
+        charFreqs.set(char, (charFreqs.get(char) || 0) + 1);
+    }
+    
+    // iterate through the rest of the words
+    for (let i = 1; i < words.length; i++) {
+        
+        let wordFreqs = new Map();
+        
+        for (let char of words[i]) {
+            
+            wordFreqs.set(char, (wordFreqs.get(char) || 0) + 1);
+        }
+        
+        // update charFreqs with the intersection of wordFreqs
+        for (let [char, freq] of charFreqs) {
+            
+            if (wordFreqs.has(char)) {
+                charFreqs.set(char, Math.min(freq, wordFreqs.get(char)));
+            }
+            
+            else {
+                
+                charFreqs.delete(char);
+            }
+        }
+    }
+    
+    // convert charFreqs to an array of characters
+    let result = [];
+    
+    for (let [char, freq] of charFreqs) {
+        
+        for (let i = 0; i < freq; i++) {
+            
+            result.push(char);
+        }
+    }
+
+    return result;
+};
 console.log(commonChars(["bella", "label", "roller"]));
