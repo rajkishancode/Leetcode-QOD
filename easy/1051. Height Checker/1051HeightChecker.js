25748d8d71940aeb568699f9409a1667d1ef498f
Raj Kishan <68591610+rajkishancode@users.noreply.github.com>
2024-06-12 23:46:13 +0530

    0075-sort-colors.js

--- easy/1051. Height Checker/1051HeightChecker.js
+++ easy/1051. Height Checker/1051HeightChecker.js
@@ -1,14 +1,24 @@
 /**
- * @param {number[]} heights
- * @return {number}
+ * @param {number[]} arr1
+ * @param {number[]} arr2
+ * @return {number[]}
  */
-var heightChecker = function (heights) {
-  let sortedHeight = [...heights].sort((a, b) => a - b);
-  let changeCount = 0;
-  for (let i = 0; i < heights.length; i++) {
-    if (heights[i] !== sortedHeight[i]) {
-      changeCount++;
+var relativeSortArray = function (arr1, arr2) {
+  // Create a map to store the order of elements in arr2
+  const orderMap = new Map();
+  arr2.forEach((num, i) => orderMap.set(num, i));
+
+  // Sort arr1 based on the order in arr2
+  arr1.sort((a, b) => {
+    // If both numbers are in arr2, sort by their order
+    if (orderMap.has(a) && orderMap.has(b)) {
+      return orderMap.get(a) - orderMap.get(b);
     }
-  }
-  return changeCount;
+    // If only one number is in arr2, that one comes first
+    if (orderMap.has(a)) return -1;
+    if (orderMap.has(b)) return 1;
+    // If neither number is in arr2, sort by their natural order
+    return a - b;
+  });
+  return arr1;
 };
