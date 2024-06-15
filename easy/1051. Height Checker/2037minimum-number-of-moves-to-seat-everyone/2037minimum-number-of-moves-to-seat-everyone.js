79d5146bf05c317b72c3c3fb7ac2ba1d03e89111
Raj Kishan <68591610+rajkishancode@users.noreply.github.com>
2024-06-15 07:46:13 +0530

    945. Minimum Increment to Make Array Unique

--- easy/1051. Height Checker/2037minimum-number-of-moves-to-seat-everyone/2037minimum-number-of-moves-to-seat-everyone.js
+++ easy/1051. Height Checker/2037minimum-number-of-moves-to-seat-everyone/2037minimum-number-of-moves-to-seat-everyone.js
@@ -7,14 +7,17 @@
 let seats = [3, 1, 5];
 let students = [2, 7, 4];
 var minMovesToSeat = function (seats, students) {
-  let count = 0;
-  const uniqueSeat = new Set(seats);
-  //   seats.sort((a, b) => a - b);
-  console.log(seats);
+  // Sort the seats and students arrays
+  seats.sort((a, b) => a - b);
+  students.sort((a, b) => a - b);
 
-  // for(let i = 0; i < students.length;i++){
-  //     if(students[i] === seats[i]) count+=0
-  //     else
-  // }
+  let totalMoves = 0;
+
+  // Calculate the sum of absolute differences
+  for (let i = 0; i < seats.length; i++) {
+    totalMoves += Math.abs(seats[i] - students[i]);
+  }
+
+  return totalMoves;
 };
 minMovesToSeat(seats, students);
